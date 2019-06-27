fee28bb9bc8478202746e81b1b0e6479cd375bd0
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-06-27 13:51:49 +0200

    Changed DB schema a bit

--- database/schema.sql
+++ database/schema.sql
@@ -1,4 +1,32 @@
-create table ECOSYSTEM (
-    id         integer primary key asc,
-    name  text not null
+CREATE TABLE ecosystem (
+    id            INTEGER PRIMARY KEY ASC,
+    name          TEXT NOT NULL,
+    description   TEXT,
+    index_url     TEXT
 );
+
+CREATE TABLE package (
+    id            INTEGER PRIMARY KEY ASC,
+    ecosystem     INTEGER NOT NULL,
+    description   TEXT,
+    package_url   TEXT,
+    repo_url      TEXT,
+    FOREIGN KEY(ecosystem) REFERENCES ecosystem(id)
+);
+
+CREATE TABLE package_index (
+    id            INTEGER PRIMARY KEY ASC,
+    ecosystem     INTEGER NOT NULL,
+    name          TEXT NOT NULL,
+    url           TEXT NOT NULL,
+    FOREIGN KEY(ecosystem) REFERENCES ecosystem(id)
+);
+
+CREATE TABLE popular_packages_index (
+    id            INTEGER PRIMARY KEY ASC,
+    ecosystem     INTEGER NOT NULL,
+    name          TEXT NOT NULL,
+    url           TEXT NOT NULL,
+    FOREIGN KEY(ecosystem) REFERENCES ecosystem(id)
+);
+
--- database/test_data.sql
+++ database/test_data.sql
@@ -1,3 +1,7 @@
-insert into ECOSYSTEM(id, name) values(1, 'Python');
-insert into ECOSYSTEM(id, name) values(2, 'Java');
-insert into ECOSYSTEM(id, name) values(3, 'JavaScript');
+INSERT INTO ecosystem(id, name, description) VALUES(1, 'Python', 'Python');
+INSERT INTO ecosystem(id, name, description) VALUES(2, 'Java', 'Java');
+INSERT INTO ecosystem(id, name, description) VALUES(3, 'JavaScript', 'JavaScript');
+
+INSERT INTO package_index(id, ecosystem, name, url) VALUES(1, 1, 'Python Package Index', 'https://pypi.org/');
+INSERT INTO package_index(id, ecosystem, name, url) VALUES(2, 2, 'Maven', 'https://maven.apache.org/');
+INSERT INTO package_index(id, ecosystem, name, url) VALUES(3, 3, 'npm', 'https://www.npmjs.com/');
