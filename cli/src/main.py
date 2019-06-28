920b313e7dc685c1a50212b8f8f9a70a5fa8271b
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-06-28 13:11:06 +0200

    CLI with prompt-toolkit like input

--- cli/src/main.py
+++ cli/src/main.py
@@ -1,4 +1,49 @@
+from prompt_toolkit import PromptSession
+from prompt_toolkit.completion import WordCompleter
+from prompt_toolkit import print_formatted_text as print, HTML
+
 from storage_interface import create_connection, select_all_ecosystems
 
 connection = create_connection("../workdir/smallsource.db")
-select_all_ecosystems(connection)
+
+
+def welcome_message():
+    print()
+    print(HTML('<ansired>Smallsource version 0.1</ansired>'))
+    print()
+    print(HTML('type <blue>quit</blue> or <blue>exit</blue> to exit'))
+    print(HTML('type <blue>help</blue> for on-line help'))
+    print(HTML('type <blue>info</blue> for the current configuration'))
+    print(HTML('type <blue>version</blue> for information about the current ' +
+               '<ansired>Smallsource</ansired> version'))
+    print()
+
+
+def show_help():
+    print("""Help
+--------
+quit - quit this application
+exit - exit from this application
+eval - evaluate
+""")
+
+
+def repl():
+    c = WordCompleter(["quit", "exit", "help", "ecosystems"], ignore_case=True)
+    s = PromptSession(completer=c)
+
+    while True:
+        cmd = s.prompt("> ")
+        if cmd in {"q", "quit", "Quit", "exit", "Exit"}:
+            break
+        elif cmd in {"help", "Help", "?"}:
+            show_help()
+        elif cmd == "ecosystems":
+            ecosystems = select_all_ecosystems(connection)
+            print(HTML('<ansired>List of ecosystems</ansired>'))
+            for ecosystem in ecosystems:
+                print(HTML('<darkblue>{}</darkblue>').format(ecosystem))
+
+
+welcome_message()
+repl()
