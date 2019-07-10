a3684e948c5b2f9c79d22821dc95ec18ca21cb6f
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-07-10 09:57:40 +0200

    Fix the issue with directory file in detect-dead-code.sh

--- qa/detect-dead-code.sh
+++ qa/detect-dead-code.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+# Script to check all Python scripts for dead code: unused imports, unused variables etc.
+
 IFS=$'\n'
-directories=$(cat directories.txt)
+
+# get the path to this script, regardless of from which directory it is called
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+# list of directories with sources to check
+directories=$(cat ${SCRIPT_DIR}/directories.txt)
+
+# go to the repo base directory
+pushd "${SCRIPT_DIR}/.."
 
 pass=0
 fail=0
@@ -40,9 +50,6 @@ function check_files() {
         fi
     done
 }
-
-SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
-pushd "${SCRIPT_DIR}/.."
 
 
 echo "----------------------------------------------------"
