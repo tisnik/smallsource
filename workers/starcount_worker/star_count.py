d0b4657be79d8214805848a40738eec1657c76f4
SamuelStuchly <sssstuchly@gmail.com>
2019-08-07 12:01:58 +0200

    commented out call of function for testing

--- workers/starcount_worker/star_count.py
+++ workers/starcount_worker/star_count.py
@@ -14,7 +14,7 @@ def do_star_count(repo):
         exit(1)
 
 # for testing
-do_star_count("electron/electrasdon")
+#do_star_count("electron/electrasdon")
 # ------------------------------------------------------------- #
 
 
