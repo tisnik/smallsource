8dc90d0a6b9f37d2bbf1d05b0f16720ae0a121d0
SamuelStuchly <sssstuchly@gmail.com>
2019-09-23 15:20:45 +0200

    functions for working with redis database added.

--- workers/data_redis.py
+++ workers/data_redis.py
@@ -4,25 +4,25 @@ import json
 r = redis.StrictRedis()
 
 
-def make_name(repository, time, worker_name, ip_address):
-    """ Compose a string to serve as ID for Json string stored in Redis """
+def make_name(repository,  worker_name, time, ip_address):
+    """ Compose a string to serve as ID for Json string stored in Redis database """
     output = str(repository) + str(worker_name) + str(time) + str(ip_address)
-    print(output)
-    return output
+
+    return output.replace(" ", "")
 
 
 def store(name, data):
-    """ Store Json sting into Redis """
-    #r.execute_command('JSON.SET', name, '.', json.dumps(data))
-    print(json.dumps(data))
+    """ Store Json sting into Redis database """
+    r.execute_command('JSON.SET', name, '.', json.dumps(data))
+
 
 
 def retrieve(name):
-    """ Get data from Redis in form of dictionary """
-    reply = json.loads(r.execute_command('JSON.GET', name))
-    print(reply)
-
-
+    """ Get data from Redis database in form of dictionary """
+    data = r.execute_command('JSON.GET', name)
+    if data is not None:
+        reply = json.loads(data)
+        return reply
 
 
 
